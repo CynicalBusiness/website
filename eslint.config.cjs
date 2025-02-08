f30bed8c772ca6ad61103fff50c35fd2693c6885
CynicalBusiness <matt@vevox.io>
2025-02-08 23:43:12 +0100

    generate new project using next.js

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,19 +1,28 @@
 const eslint = require("@eslint/js");
 const tseslint = require("typescript-eslint");
-// const eslintImport = require("eslint-plugin-import");
 const stylistic = require("@stylistic/eslint-plugin");
 const unusedImports = require("eslint-plugin-unused-imports");
 const eslintConfigPrettier = require("eslint-config-prettier");
+const importPlugin = require("eslint-plugin-import");
 
 module.exports = [
     eslint.configs.recommended,
     ...tseslint.configs.recommended,
-    // eslintImport.flatConfigs.recommended,
-    // eslintImport.flatConfigs.typescript,
     {
         plugins: { "unused-imports": unusedImports },
         rules: {
             "unused-imports/no-unused-imports": "error",
+        },
+    },
+
+    importPlugin.flatConfigs.recommended,
+    importPlugin.flatConfigs.typescript,
+    {
+        settings: {
+            "import/resolver": {
+                typescript: true,
+                node: true,
+            },
         },
     },
 
@@ -33,6 +42,8 @@ module.exports = [
                 "double",
                 { avoidEscape: true, allowTemplateLiterals: true },
             ],
+            "import/extensions": ["error", "ignorePackages"],
+            "import/no-unresolved": "off", // handled by typescript
         },
     },
     {
---
+++ packages/apps/web/next.config.ts
@@ -0,0 +1,19 @@
+// @ts-check
+
+import { composePlugins, withNx } from "@nx/next";
+import type { WithNxOptions } from "@nx/next/plugins/with-nx";
+
+const nextConfig: WithNxOptions = {
+    nx: {
+        // Set this to true if you would like to use SVGR
+        // See: https://github.com/gregberge/svgr
+        svgr: false,
+    },
+};
+
+const plugins = [
+    // Add more Next.js plugins to this list if needed.
+    withNx,
+];
+
+module.exports = composePlugins(...plugins)(nextConfig);
