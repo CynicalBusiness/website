1385d552c1a7367a9b2fff8aedc2a7663393f736
CynicalBusiness <matt@vevox.io>
2025-04-04 02:20:08 +0200

    sticky main nav

--- src/components/layout/main-nav.tsx
+++ src/components/layout/main-nav.tsx
@@ -5,7 +5,7 @@ export function MainNav() {
     return (
         <nav
             id="mainNav"
-            className="container flex flex-col lg:flex-row items-center justify-center py-2 lg:py-4 gap-x-12 mb-4"
+            className="container flex flex-col lg:flex-row items-center justify-center py-2 lg:py-4 gap-x-12 mb-4 sticky top-0 z-10 bg-background/90 shadow-lg shadow-background/90"
         >
             <Link
                 to="/"
